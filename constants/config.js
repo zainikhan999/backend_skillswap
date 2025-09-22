1af30eae90e7f40a8526f97598a8913faa2c6feb
zainikhan999 <zk.032918@gmail.com>
2025-09-23 00:29:10 +0500

    backend deployment

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "https://skillswap-frontend-ten.vercel.app/",
+  origin: "https://skillswap-frontend-ten.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
