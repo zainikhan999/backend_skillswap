6c9a3e400139e4c20a8970b27aaa405015145dc8
zainikhan999 <zk.032918@gmail.com>
2025-10-18 18:26:29 +0500

    cookiesresolved

--- utils/cookieUtils.js
+++ utils/cookieUtils.js
@@ -3,10 +3,10 @@ export const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "DEPLOYMENT" ? true : false, // ⚠ important for localhost
-    secure: false, // ⚠ important for localhost
+    // secure: false, // ⚠ important for localhost
 
-    // sameSite: "none", // 'lax' or 'strict' for local dev, 'none' for production
-    sameSite: "lax", // 'lax' or 'strict' for local dev, 'none' for production
+    sameSite: "none", // 'lax' or 'strict' for local dev, 'none' for production
+    // sameSite: "lax", // 'lax' or 'strict' for local dev, 'none' for production
 
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
